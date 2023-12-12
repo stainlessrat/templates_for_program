61e10deb2a8d65679d41c7235d10ba0b2300b0d0
Sergey Stroev <stainlessrat83@gmail.com>
2023-12-12 10:14:42 +0700

    add logging

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
+import logging
+
 from fastapi import FastAPI, status
+
+from app import log
+
+log.config_log('web.json')
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
 @app.post('/')
 async def main():
+    _logger = logging.LoggerAdapter(logger, {'some_user_key': 'test'})
+    _logger.debug('Call main function')
+    _logger.info('Main function finish')
     return {'msg': 'ok'}
 
 
